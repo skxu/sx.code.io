25ebbe0f0717fcb7a800df8d08f65fc0bcf6a9c3
SAAAAAAAAAM <skx@berkeley.edu>
2014-12-20 16:03:01 -0800

    added about section

--- s_x.js
+++ s_x.js
@@ -62,12 +62,14 @@ states = {
   
   About: [
     {
+      type:'markdown',
       name:'Coming Soon',
       data:{
-        text: '##Test  \n 1234'
+        text: '##S\\_X\nS\\_X is a pseudo-fansub group. Our goals:  \n\n* Provide a \'watchable\' show in reasonable time\n* Release within 4 hours of airing \n* Fix wrong translations \n* Edit awkward phrases & grammar \n* Reduce as many 2-liners into 1-liners as possible\n  * May involve re-timing / typesetting \n* Include OP&ED from other groups, if available\n* Chapter the releases  \n\nS\\_X releases are meant to be fast & watched during the airing season, not high quality archive material.  \n##Q\\_X \nQ\\_X is a subdivision which doesn\'t actually exist yet. While S\\_X provides speed, Q\\_X has the goal of providing more quality:  \n\n* Releases are expected within 48 hours \n* Typesetting where appropriate - e.g. signs \n* More editing with QC \n* Better encoding  \n\n##Website \nWe don\'t like Wordpress so our site is a bit different.   \n  \n<u>**Hosting**</u>: DigitalOcean ($60/yr)  \n<u>**Domain**</u>: nic.io ($60/yr)  \n<u>**Webserver**</u>: Nginx  \n<u>**Site**</u>:ReactJS  \n<u>**Blog**</u>:Ghost  \n\n##FAQ \n***Why do you steal other groups\' kfx?***  \nThe goal of S_X is and has always been to release fast. We don\'t have time or manpower to do kfx. Also, we always give credit to the other groups.  \n\n***Why do your subs suck?***  \nWe\'re new & learning. Criticism is greatly appreciated since it\'s not always easy seeing your own faults.  \n\n***Why is your website so shitty?***  \nSomeone had the wonderful idea of building it from scratch.  \n\n***There are so many fansub groups out there already. Why bother?***  \n We\'re doing this for ourselves. Also, half the groups take too long to release or just quit mid-season.  \n\n'
       }
     }
   ]
+  
   
 };
 
@@ -128,17 +130,7 @@ var StoryList = React.createClass({
                   <div className={item.name}><p className="episode" >EP {ep.ep}: <a href={ep.url}>Torrent</a> | No DDL</p></div>
                 );
               });
-          
-          
-          
-          
-          }
-          
-          
-          
-          return (
-                
-  
+            return (
                 <tr key={item.data.score}>
                     <td>
                         <p className="score">{item.data.score}</p>
@@ -167,20 +159,30 @@ var StoryList = React.createClass({
                         </p>
             
                         <p className="line">
-                          {item.data.episodes !== undefined ? <button className="ep_button" onClick={function() {$("[class='"+item.name+"']").slideToggle(0)}}>Episode List</button> : ""}
+                          {item.data.episodes !== undefined ? <button className="ep_button" onClick={function() {$("[class='"+item.name+"']").slideToggle(0)}}>Episode List</button> : ""
+        }
                         </p>
                                               
-                      </td>
-                      <td>
+                    </td>
+                    <td>
                         <p>
                           {storyDetails}
                         </p>
               
-                        
                     </td>
                 </tr>
             );
-        });
+        
+        } else if (item.type === 'markdown') {
+          var rawMarkup = converter.makeHtml(item.data.text);
+          return (
+            <div dangerouslySetInnerHTML={{__html: rawMarkup}} />
+          );
+  
+  
+        }
+              
+    }); //end storynodes
       
       
 
