41d9a2c84030f2d2cf33734753250776d07df2f2
SAAAAAAAAAM <skx@berkeley.edu>
2014-12-21 16:58:18 -0800

    homepage

--- s_x.js
+++ s_x.js
@@ -48,15 +48,31 @@ states = {
   
   Home: [
     {
+      type:'news',
       name:'Welcome',
-      data:{}
+      data:{
+        stories: [
+          {
+            date:'12-21-14',
+            text:'##Happy Holidays\n  \n\n\nMost recent Trinity Seven episode: 11, [torrent](http://www.nyaa.se/?page=download&tid=635424)  \n\n<img src="http://cdn.awwni.me/mv27.jpg" alt="Drawing" style="width: 35%;"/>'
+          },
+          /*
+          {
+            date:'12-20-14',
+            text:'##Site Updates  \n About, Staff, & Homepage have are updated!'
+          }*/
+        ]
+      }
     }
   ],
   
   Staff: [
     {
+      type:'markdown',
       name:'Coming Soon',
-      data:{}
+      data:{
+        text: 'Hm?  \n <img src="http://i.imgur.com/hzbuKUd.png" alt="Drawing" style="width: 50%;"/>'
+      }
     }
   ],
   
@@ -65,7 +81,7 @@ states = {
       type:'markdown',
       name:'Coming Soon',
       data:{
-        text: '##S\\_X\nS\\_X is a pseudo-fansub group. Our goals:  \n\n* Provide a \'watchable\' show in reasonable time\n* Release within 4 hours of airing \n* Fix wrong translations \n* Edit awkward phrases & grammar \n* Reduce as many 2-liners into 1-liners as possible\n  * May involve re-timing / typesetting \n* Include OP&ED from other groups, if available\n* Chapter the releases  \n\nS\\_X releases are meant to be fast & watched during the airing season, not high quality archive material.  \n##Q\\_X \nQ\\_X is a subdivision which doesn\'t actually exist yet. While S\\_X provides speed, Q\\_X has the goal of providing more quality:  \n\n* Releases are expected within 48 hours \n* Typesetting where appropriate - e.g. signs \n* More editing with QC \n* Better encoding  \n\n##Website \nWe don\'t like Wordpress so our site is a bit different.   \n  \n<u>**Hosting**</u>: DigitalOcean ($60/yr)  \n<u>**Domain**</u>: nic.io ($60/yr)  \n<u>**Webserver**</u>: Nginx  \n<u>**Site**</u>:ReactJS  \n<u>**Blog**</u>:Ghost  \n\n##FAQ \n***Why do you steal other groups\' kfx?***  \nThe goal of S_X is and has always been to release fast. We don\'t have time or manpower to do kfx. Also, we always give credit to the other groups.  \n\n***Why do your subs suck?***  \nWe\'re new & learning. Criticism is greatly appreciated since it\'s not always easy seeing your own faults.  \n\n***Why is your website so shitty?***  \nSomeone had the wonderful idea of building it from scratch.  \n\n***There are so many fansub groups out there already. Why bother?***  \n We\'re doing this for ourselves. Also, half the groups take too long to release or just quit mid-season.  \n\n'
+        text: '##S\\_X\nS\\_X is a pseudo-fansub group. Our goals:  \n\n* Provide a \'watchable\' show in reasonable time\n* Release within 4 hours of airing \n* Fix wrong translations \n* Edit awkward phrases & grammar \n* Reduce as many 2-liners into 1-liners as possible\n  * May involve re-timing / typesetting \n* Include OP&ED from other groups, if available\n* Chapter the releases  \n\nS\\_X releases are meant to be fast & watched during the airing season, not high quality archive material.  \n<br></br>\n##Q\\_X \nQ\\_X is a subdivision which doesn\'t actually exist yet. While S\\_X provides speed, Q\\_X has the goal of providing better quality:  \n\n* Releases are expected within 48 hours \n* Typesetting where appropriate - e.g. signs \n* More editing with QC \n* Better encoding  \n\n<br></br>\n##Website \nWe don\'t like Wordpress so our site is a bit different. You can view the source code [here](https://github.com/skxu/sx.code.io).  \n  \n<u>**Hosting**</u>: DigitalOcean ($60/yr)  \n<u>**Domain**</u>: nic.io ($60/yr)  \n<u>**Webserver**</u>: Nginx  \n<u>**Site**</u>:ReactJS  \n<u>**Blog**</u>:Ghost  \n\n<br></br>\n##FAQ \n***Why do you steal other groups\' kfx?***  \nThe goal of S_X is and has always been to release fast. We don\'t have time or manpower to do kfx. Also, we always give credit to the other groups.  \n\n***Why do your subs suck?***  \nWe\'re new & learning. Criticism is greatly appreciated since it\'s not always easy seeing your own faults.  \n\n***Why is your website so shitty?***  \nSomeone had the wonderful idea of building it from scratch.  \n\n***There are so many fansub groups out there already. Why bother?***  \n We\'re doing this for ourselves. Also, half the groups take too long to release or just quit mid-season.  \n\n'
       }
     }
   ]
@@ -122,7 +138,7 @@ var StoryList = React.createClass({
   
     render: function() {
         var storyNodes = this.props.items.map(function(item) {
-          console.log(item);
+          //console.log(item);
           var storyDetails;
           if (item.type === 'TV') {
             storyDetails = item.data.episodes.map(function(ep) {
@@ -184,6 +200,26 @@ var StoryList = React.createClass({
           );
   
   
+        } else if (item.type === 'news') {
+          
+          storyDetails = item.data.stories.map(function(story) {
+            var rawMarkup = converter.makeHtml(story.text);
+            return (
+              <tr key={story.date}>
+                <td>
+                    <p>
+                      <div dangerouslySetInnerHTML={{__html: rawMarkup}} />
+                    </p>
+                    <br></br>
+                </td>
+              </tr>
+            );
+          });
+          
+          return (
+            <div>{storyDetails}</div>
+          );
+        
         }
               
     }); //end storynodes
